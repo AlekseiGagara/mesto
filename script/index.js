4840090cf81aaf0f99854c2543ea864af334ec59
Aleksei Gagara <agagara90@gmail.com>
2023-04-13 08:32:48 +1000

    fix: замена png на svg, переделка form и popup

--- script/index.js
+++ script/index.js
@@ -1,9 +1,9 @@
 const editButton = document.querySelector(".user-profile__edit-button");
 const closeButton = document.querySelector(".popup__close-button");
 const popup = document.querySelector(".popup");
-const popupForm = document.querySelector(".popup__container");
-const inputUsername = document.querySelector(".popup__username");
-const inputDescription = document.querySelector(".popup__description");
+const popupForm = document.querySelector(".popup__form");
+const inputUsername = document.querySelector(".popup__input_username");
+const inputDescription = document.querySelector(".popup__input_description");
 const userProfileName = document.querySelector(".user-profile__name");
 const userProfileDescription = document.querySelector(".user-profile__description");
 
@@ -15,20 +15,15 @@ function openPopup () {
 
 function closePopup () {
   popup.classList.remove("popup_opened");
-  inputUsername.value = userProfileName.textContent;
-  inputDescription.value = userProfileDescription.textContent;
 };
-
-editButton.addEventListener("click", openPopup);
-closeButton.addEventListener("click", closePopup);
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
-    inputUsername.textContent = inputUsername.value;
-    inputDescription.textContent = inputDescription.value;
     userProfileName.textContent = inputUsername.value;
     userProfileDescription.textContent = inputDescription.value;
     closePopup ();
 }
 
+editButton.addEventListener("click", openPopup);
+closeButton.addEventListener("click", closePopup);
 popupForm.addEventListener('submit', handleFormSubmit);
