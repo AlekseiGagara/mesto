e49c4348c54e46868d3b5f6e3faaf6ce46035e9d
Aleksei Gagara <agagara90@gmail.com>
2023-05-02 05:14:50 +1000

    fix: создать createCard(data) с обраб. Zoom внутри

--- script/index.js
+++ script/index.js
@@ -3,7 +3,6 @@ const editButton = document.querySelector(".user-profile__edit-button");
 const addCardButton = document.querySelector(".user-profile__add-button");
 const popupCloseButtons = document.querySelectorAll(".popup__close-button");
 // переменные для popup'ов и input'ов
-const popup = document.querySelector(".popup");
 const popupAddCard = document.querySelector(".popup_add-card");
 const popupEditProfile = document.querySelector(".popup_edit");
 const popupZoomImage = document.querySelector(".popup_zoom-image");
@@ -24,68 +23,84 @@ const cardsContainer = document.querySelector(".places");
 const initialCards = [
   {
     name: "Карачаевск",
-    link: "https://alekseigagara.github.io/mesto/images/photo-grid-karachaevsk.jpg"
+    link: "https://alekseigagara.github.io/mesto/images/photo-grid-karachaevsk.jpg",
   },
   {
     name: "Гора Эльбрус",
-    link: "https://alekseigagara.github.io/mesto/images/photo-grid-elbrus.jpg"
+    link: "https://alekseigagara.github.io/mesto/images/photo-grid-elbrus.jpg",
   },
   {
     name: "Домбай",
-    link: "https://alekseigagara.github.io/mesto/images/photo-grid-dombai.jpg"
+    link: "https://alekseigagara.github.io/mesto/images/photo-grid-dombai.jpg",
   },
   {
     name: "Гранд-Каньон",
-    link: "https://alekseigagara.github.io/mesto/images/photo-grid-grand_canyon.jpg"
+    link: "https://alekseigagara.github.io/mesto/images/photo-grid-grand_canyon.jpg",
   },
   {
     name: "Каппадокия",
-    link: "https://alekseigagara.github.io/mesto/images/photo-grid-capadocia.jpg"
+    link: "https://alekseigagara.github.io/mesto/images/photo-grid-capadocia.jpg",
   },
   {
     name: "Большое Алматинское озеро",
-    link: "https://alekseigagara.github.io/mesto/images/photo-grid-BAO.jpg"
+    link: "https://alekseigagara.github.io/mesto/images/photo-grid-BAO.jpg",
   },
 ];
+//функция создания новой карточки с обработчиком для Zoom по клику на фото
+function createCard(data) {
+  const cardElement = placeTemplate.content.cloneNode(true);
+  const placeTitle = cardElement.querySelector(".place__title");
+  const placeImage = cardElement.querySelector(".place__image");
+  placeTitle.textContent = data.name;
+  placeImage.src = data.link;
+  placeImage.alt = data.name;
+  placeImage.addEventListener("click", () => {
+    popupImage.src = data.link;
+    popupImage.alt = data.name;
+    popupImageCaption.textContent = data.name;
+    openPopup(popupZoomImage);
+  });
+  return cardElement;
+}
 
 // цикл для автоматического добавления стартовых карточек с использованием информации из массива
-initialCards.forEach(function (card) {
-  // дублируем узел
-  const placeElement = placeTemplate.content.cloneNode(true);
-
-  // получаем элементы внутри карточки
-  const placeTitle = placeElement.querySelector(".place__title");
-  const placeImage = placeElement.querySelector(".place__image");
-
-  // присваиваем необходимые данные из массива
-  placeTitle.textContent = card.name;
-  placeImage.src = card.link;
-  placeImage.alt = card.name;
-
+initialCards.forEach(function (cardData) {
+  const cardElement = createCard(cardData);
   // добавляем карточку в начало контейнера
-  cardsContainer.prepend(placeElement);
+  cardsContainer.prepend(cardElement);
 });
 
-//open popup
-editButton.addEventListener("click", () => openPopup(popupEditProfile));
-addCardButton.addEventListener("click", () => openPopup(popupAddCard));
-cardsContainer.addEventListener("click", evt => {
-  if (evt.target.classList.contains("place__image")) {
-    const card = evt.target.closest(".place");
-    const cardTitle = card.querySelector(".place__title").textContent;
-    popupImage.src = evt.target.src;
-    popupImage.alt = evt.target.alt;
-    popupImageCaption.textContent = cardTitle;
-    popupZoomImage.classList.add ("popup_opened");
-  }
-});
-//функция открытия popup
-function openPopup (popupType) {
-  popupType.classList.add ("popup_opened");
+//функция добавления новой карточки на страницу
+function handleAddCardFormSubmit(evt) {
+  evt.preventDefault();
+  //создаем объект с данными для функции createCard
+  const cardData = {
+    name: inputPlaceTitle.value,
+    link: inputPlaceImageLink.value,
+  };
+  const cardElement = createCard(cardData);
+  // добавляем карточку в начало контейнера
+  cardsContainer.prepend(cardElement);
+  //закрываем popup
+  const popup = evt.target.closest(".popup");
+  closePopup(popup);
+}
+
+//слушатель для open popup EditProfile
+editButton.addEventListener("click", () => {
+  openPopup(popupEditProfile);
+  //присваиваем данные инпутам из текущей информации профиля
   inputUsername.value = userProfileName.textContent;
   inputDescription.value = userProfileDescription.textContent;
-};
+});
 
+//слушатель open popup AddCard
+addCardButton.addEventListener("click", () => openPopup(popupAddCard));
+
+//функция открытия popup
+function openPopup(popupType) {
+  popupType.classList.add("popup_opened");
+}
 
 //цикл для кнопок закрытия, где выбирается ближайший popup к слушателю
 popupCloseButtons.forEach((button) => {
@@ -96,7 +111,7 @@ popupCloseButtons.forEach((button) => {
 });
 
 //функция редактирования профиля
-function handleEditFormSubmit (evt) {
+function handleEditFormSubmit(evt) {
   evt.preventDefault();
   //присваиваем значения из инпутов
   userProfileName.textContent = inputUsername.value;
@@ -105,42 +120,24 @@ function handleEditFormSubmit (evt) {
   const popup = evt.target.closest(".popup");
   closePopup(popup);
 }
-//функция добавления новой карточки на страницу
-function handleAddCardFormSubmit (evt) {
-  evt.preventDefault();
-  //дублируем узел и выбираем необходимые элементы
-  const placeElement = placeTemplate.content.cloneNode(true);
-  const placeTitle = placeElement.querySelector(".place__title");
-  const placeImage = placeElement.querySelector(".place__image");
-  //присваиваем значения из инпутов
-  placeTitle.textContent = inputPlaceTitle.value;
-  placeImage.src = inputPlaceImageLink.value;
-  placeImage.alt = inputPlaceTitle.value;
-  // добавляем карточку в начало контейнера
-  cardsContainer.prepend(placeElement);
-  //закрываем popup
-  const popup = evt.target.closest(".popup");
-  closePopup(popup);
-}
 
 //close popup
-function closePopup (popupType) {
+function closePopup(popupType) {
   popupType.classList.remove("popup_opened");
-};
-
+}
 
 //слушатели события на кнопках submit
 editForm.addEventListener("submit", handleEditFormSubmit);
 addForm.addEventListener("submit", handleAddCardFormSubmit);
 
 //функция и слушатель для лайков
-cardsContainer.addEventListener("click", evt => {
+cardsContainer.addEventListener("click", (evt) => {
   if (evt.target.classList.contains("place__like-button")) {
     evt.target.classList.toggle("place__like-button_like-active");
   }
 });
 //функция и слушатель для удаления
-cardsContainer.addEventListener("click", evt => {
+cardsContainer.addEventListener("click", (evt) => {
   if (evt.target.classList.contains("place__delete-button")) {
     const placeElement = evt.target.closest(".place");
     placeElement.remove();
